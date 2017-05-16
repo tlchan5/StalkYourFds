02040b655c36a326015a0d9199707c011d2b7502
Gordon Cheung <erc@mail.org>
2017-05-16 19:13:18 +0800

    Beacon Controller class

---
+++ app/src/main/java/com/example/csci3310gp28/stalkyourfds/CallbackInterface.java
@@ -0,0 +1,13 @@
+package com.example.kalok.testbeacon;
+
+import com.aprilbrother.aprilbrothersdk.Beacon;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kalok on 15/05/2017.
+ */
+
+public interface CallbackInterface{
+    void onRanged(ArrayList<Beacon> beacons);
+}
